df112f9c5097dcade8529351d3fbfff7fe76a6fe
Amine Karimi <aminekarimi1998@gmail.com>
2025-01-30 16:06:30 +0100

    add flow for Queries

--- firebase-database-ktx/src/main/kotlin/com/skydoves/firebase/database/ktx/DatabaseCoroutines.kt
+++ firebase-database-ktx/src/main/kotlin/com/skydoves/firebase/database/ktx/DatabaseCoroutines.kt
@@ -21,11 +21,36 @@ import com.google.firebase.database.ChildEventListener
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.Query
 import com.google.firebase.database.ValueEventListener
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
 import org.json.JSONObject
+
+/**
+ * Returns a flow that emits a target object [T] from the specified [path] within the [DatabaseReference].
+ *
+ * The flow emits a [Result], encapsulating both the snapshot value and any potential errors.
+ */
+public fun <T : Any> Query.flow(
+  path: (DataSnapshot) -> DataSnapshot,
+  decodeProvider: (String) -> T,
+): Flow<Result<T?>> = callbackFlow {
+  val listener = object : ValueEventListener {
+    override fun onDataChange(snapshot: DataSnapshot) {
+      val data = path.invoke(snapshot).serializedValue(decodeProvider)
+      trySend(Result.success(data))
+    }
+
+    override fun onCancelled(error: DatabaseError) {
+      trySend(Result.failure(error.toException()))
+    }
+  }
+  addValueEventListener(listener)
+
+  awaitClose { removeEventListener(listener) }
+}
 
 /**
  * Returns a flow that emits a target object [T] from the specified [path] within the [DatabaseReference].
